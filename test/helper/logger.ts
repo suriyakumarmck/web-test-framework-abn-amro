c4c31d44c6b380516b97beef99b53cb872681cb5
suriyakumarmck <suriyakumar.m.c@gmail.com>
2022-04-16 03:48:25 +0200

    Added file logger via winston

--- test/helper/logger.ts
+++ test/helper/logger.ts
@@ -1,6 +1,14 @@
 import winston from "winston"
 
 // Format console.log
+let mydate = new Date();
+let Filename = './logs/' + mydate.getFullYear() + "-" + mydate.getMonth() + "-" + mydate.getDate() + "-" + mydate.getHours() + ':' + mydate.getMinutes() + ':' + mydate.getSeconds()+ "-combined.log";
+
+const fileFormat = winston.format.printf(({ level, message, timestamp }) => {
+    const logLevel = winston.format.colorize().colorize(level, `${level.toUpperCase()}`)
+    return `${timestamp} [${logLevel}]: ${message}`;
+  });
+
 const consoleFormat = winston.format.printf(({ level, message
 }) => {
     const logLevel = winston.format.colorize().colorize(level, `${level.toUpperCase()}`)
@@ -13,7 +21,13 @@ let logger = winston.createLogger({
             level: process.env.LOG_LEVEL,
             handleExceptions: true,
             format: winston.format.combine(winston.format.timestamp(), consoleFormat)
-        })
+        }),
+        new winston.transports.File({ 
+            filename: Filename, 
+            level: process.env.LOG_LEVEL,
+            handleExceptions: true,
+            format: winston.format.combine(winston.format.timestamp(), fileFormat)
+         })
     ]
 })
 // Print any unknown error
